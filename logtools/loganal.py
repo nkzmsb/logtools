6466b74589276083d98ac5b7b27184a11b12bc47
nkzmsb <mkzmsb.aou@gmail.com>
2021-05-09 00:14:26 +0900

    listup functions @loganal

---
+++ logtools/loganal.py
@@ -0,0 +1,37 @@
+
+
+def breakdown_values():
+    return {"aa":0}
+
+
+def log_to_dict(unitlog_str)->dict:
+    # values以外をdict化
+    # valuesをbreakdown_values()で分解
+    # valuesの内容をdictに追加
+    return {"aa":0}
+
+def logfile_converter(filepath)->"list of dict":
+    # log_to_dict()をループ
+    ...
+    
+def renamefiles(dirpath):
+    # フォルダ内の**.log#を**_#.logに変換
+    # 変換後のファイル名のリストをreturn
+    ...
+
+
+class LogData():
+    def __init__(self):
+        # renamefiles()
+        # logfile_converterのループ
+        # self.log_dfの作成
+        ...
+        
+    @property
+    def log_df(self):
+        # self.log_dfのコピーを返す
+        ...
+        
+    # [FutureWork]
+    # def export_db(self):
+    #     ...
--- tests/test_loganal.py
+++ tests/test_loganal.py
@@ -1,7 +1,7 @@
 
 import pytest
 
-from logtools.loganal import unitlog_converter
+from logtools.loganal import log_to_dict
 from logtools.default import default
 
 DEFAULT = default()
@@ -23,20 +23,41 @@ def test_dummylogs(valid_log):
                          "'nest': {'A': 'nestA', "
                          "'BB': {'bnest': [1, 2, 3], 'tag': True}}}")
 
-# def test_unitlog_converter(valid_log):
-#     expect = {"asctime":"2021-05-08 21:57:23,823"
-#               , "levelname" : "INFO"
-#               , "name" : "DUMMYLOG"
-#               , "func" : "FUNC"
-#               , "action":"run"
-#               , "exception":None
-#               , "message" : "d_message"
-#               , "tag":None
-#               , "A":"AAA"
-#               , "int" : 3
-#               }
-#     
-#     assert unitlog_converter(valid_log) == expect
+
+def test_breakdown_values():
+    ...
+
+
+def test_log_to_dict(valid_log):
+    expect = {"asctime":"2021-05-08 21:57:23,823"
+              , "levelname" : "INFO"
+              , "name" : "DUMMYLOG"
+              , "func" : "FUNC"
+              , "action":"run"
+              , "exception":None
+              , "message" : "d_message"
+              , "tag":None
+              , "A":"AAA"
+              , "int" : 3
+              , "nest-A" : "nestA"
+              , "nest-BB-bnest" : [1, 2, 3]
+              , "nest-BB-tag" : True
+              }
+    
+    assert log_to_dict(valid_log) == expect
+    
+def test_log_to_dict_ex():
+    # エラーメッセージを処理できるかどうか
+    ...
+
+
+# [ToDo]以下の項目でExceptionのテストが必要
+# - そもそも入りが違う(@log_to_dict)
+# - 項目の数が違う(@log_to_dict)
+# - valuesがdictではない(@breakdown_values)
+# - ast.literal_evalのSyntaxError(@breakdown_values)
+# - ログファイルが見つからない(@renamefiles)
+
 
 
 # ===参考
