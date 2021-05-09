12304350be0861ff87034d01f258759d70db0943
nkzmsb <mkzmsb.aou@gmail.com>
2021-05-09 10:45:27 +0900

    prepare breakdow_value tests

--- tests/test_loganal.py
+++ tests/test_loganal.py
@@ -35,7 +35,22 @@ def test_breakdown_values():
     
     assert breakdown_values(values) == expect
 
+def test_breakdown_values_notdict():
+    assert breakdown_values(5) == {"values" : 5}
+    
+def test_breakdown_values_warning_SE(recwarn):
+    values = "{'int': 3, 'ndarray': array([[1, 2, 3],"
+    
+    breakdown_values(values)
+    
+    assert len(recwarn) == 1
+    
+    w = recwarn.pop()
+    assert w.category(Warning)
+    assert str(w.message)==("values is not valid type")
 
+
+@pytest.mark.skip(reason="未実装")
 def test_log_to_dict(valid_typ_log):
     expect = {"asctime":"2021-05-08 21:57:23,823"
               , "levelname" : "INFO"
@@ -54,11 +69,12 @@ def test_log_to_dict(valid_typ_log):
     
     assert log_to_dict(valid_typ_log) == expect
 
+@pytest.mark.skip(reason="未実装")
 def test_logfile_converter():
     # データとexpectの準備が面倒くさい
     ...
 
-
+@pytest.mark.skip(reason="未実装")
 def test_rename():
     # 一時フォルダにlogdata.log, logdata.log2, logdata.log3, dummy.txtを準備する
     # 処理後に、logdata_1.log, logdata_2.log, logdata_3.log, dummy.txtになっていることを確認する
@@ -67,10 +83,11 @@ def test_rename():
     
 
 # [ToDo]以下の項目でExceptionのテストが必要
+# Warning
 # - そもそも入りが違う(@log_to_dict)
 # - 項目の数が違う(@log_to_dict)
-# - valuesがdictではない(@breakdown_values)
-# - ast.literal_evalのSyntaxError(@breakdown_values)
+# - /ast.literal_evalのSyntaxError(@breakdown_values)
+# Exception
 # - ログファイルが見つからない(@renamefiles)
 
 
