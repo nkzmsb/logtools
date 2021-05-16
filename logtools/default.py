dc9cb8f85c3c3a20a23bebed6ab7a0f9fb24a58a
nkzmsb <mkzmsb.aou@gmail.com>
2021-05-16 17:49:01 +0900

    update default dict format

--- logtools/default.py
+++ logtools/default.py
@@ -2,20 +2,57 @@
 # コードの冗長性をなくす目的のモジュール。積極的な変更は想定しない。
 # 基本的にはこのフォーマットを使うため、テストはこのデフォルトフォーマットに対してのみ行う
 
-# "asctime", "levelname", "name", "message"は組み込み
-# それ以外はextra引数で指定
-attributes = tuple(["asctime", "levelname", "name", "func"
+# ログに含める属性
+# この順番通りにログが作成される
+ATTRIBUTES = tuple(["asctime", "levelname", "name", "func"
                     , "action", "exception", "message", "tag", "values"
                     ])
 
 # 各属性のスプリッター
-splitter = "___"
+SPLITTER = "___"
 
-formatter = ""
-for i, attrib in enumerate(attributes):
-    formatter += "%({})s".format(attrib)
-    if i < len(attributes) - 1:
-        formatter += splitter
+FORMATTER = ""
+
+#####################################################
+# ここまでが設定
+# ここから下はルーチン処理
+#####################################################
+
+# 用意されている属性
+# https://docs.python.org/ja/3/library/logging.html#logrecord-attributes
+ATTRIBUTE_BUILT_IN_ALL = ["asctime", "created", "filename", "funcName"
+                          , "levelname", "levelno", "message", "module"
+                          , "msecs", "name", "pathname", "process", "processName"
+                          , "relativeCreated", "thread", "threadName"]
+
+
+def classified_attr():
+    """ATTRIBUTESをATTRIBUTE_BUILT_IN_ALLに含まれるものとそうでないものに分類する
+
+    Returns
+    -------
+    attribute_buildin： tuple
+        ATTRIBUTE_BUILT_IN_ALLに含まれる属性
+    attribute_extra: tuple
+        ATTRIBUTE_BUILT_IN_ALLに含まれない属性
+    """
+    all_use_set = set(ATTRIBUTES)
+    all_builtin_set = set(ATTRIBUTE_BUILT_IN_ALL)
+    
+    attribute_buildin = tuple(sorted(all_use_set&all_builtin_set
+                                     , key = ATTRIBUTES.index))
+    attribute_extra = tuple(sorted(all_use_set-all_builtin_set
+                                   , key = ATTRIBUTES.index))
+    
+    return attribute_buildin, attribute_extra
+
+
+attribute_buildin, attribute_extra = classified_attr()
+
+for i, attrib in enumerate(ATTRIBUTES):
+    FORMATTER += "%({})s".format(attrib)
+    if i < len(ATTRIBUTES) - 1:
+        FORMATTER += SPLITTER
 
 def default():
     """デフォルト値を返す
@@ -24,12 +61,22 @@ def default():
     formatter : ロギングのフォーマット。ハンドラにsetされることを想定している。
     splitter : ロギング属性を分けるスプリッター
     """
-    dic = {"attributes" : attributes
-           , "formatter" : formatter
-           , "splitter" : splitter}
+    dic = {"attributes" : ATTRIBUTES
+           , "attribute_buildin" : attribute_buildin
+           , "attribute_extra" : attribute_extra
+           , "formatter" : FORMATTER
+           , "splitter" : SPLITTER}
     return dic
 
+
 if __name__ == "__main__":
-    de = default()
-    print("attributes : ", de["attributes"])
-    print("formatter : ", de["formatter"])
+    l1 = (5,-4,1,2,-2,3,10)
+    s1 = set(l1)
+    
+    l2 = [6,5,7,4,8,3,9,2,1]
+    s2 = set(l2)
+    
+    print(sorted(s1&s2, key = l1.index))
+    print(sorted(s1-s2, key = l1.index))
+    
+    print(default())
