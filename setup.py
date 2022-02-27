dce0f305a89d56b42bdfbb5153b038d07e82428c
nkzmsb <mkzmsb.aou@gmail.com>
2022-02-27 23:04:09 +0900

    docs: prepare for v1.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "logtools"
-    , version = "0.1.0"
+    , version = "1.0.0"
     , packages = find_packages()
     , zip_safe=False
     
